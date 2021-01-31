e13f4b87fa3356ceccf0b0f6b5db58e18a2849d6
piersto <44554156+piersto@users.noreply.github.com>
2021-01-31 13:27:49 -0500

    Добавлено несколько функций

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world! РАЗ два три");
+        hello("world");
+        hello("user");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len  + "=" + area(len));
+
+        double a = 5;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
+
+    }
+
+    public static void hello(String somebody){
+
+        System.out.println("Hello, " + somebody + "! РАЗ два три! € ");
+    }
+
+    public static double area (double l){
+        return l *  l;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }
