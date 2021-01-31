8898888a51f9f8a071c988ae1ab0b7a9df0637fe
piersto <44554156+piersto@users.noreply.github.com>
2021-01-31 12:52:39 -0500

    Внёс изменения в build.gradle

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,6 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world! РАЗ два три");
     }
 }
