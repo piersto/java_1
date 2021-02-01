c7fdd9bb602aeb20852f104a0e0228064c58aad9
piersto <44554156+piersto@users.noreply.github.com>
2021-02-01 16:21:39 -0500

    Added testArea

---
+++ sendbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+import org.junit.Test;
+
+public class SquareTests {
+
+    @Test
+    public void testArea(){
+        Square s = new Square(5);
+        assert s.area() == 25;
+
+    }
+
+}
