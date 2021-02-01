3add3310256554dec065cd4ca27d2d137ffbdbd6
piersto <44554156+piersto@users.noreply.github.com>
2021-02-01 16:51:22 -0500

    Added Assert.assertEquals

--- sendbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sendbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.sandbox;
 
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SquareTests {
 
     @Test
     public void testArea(){
         Square s = new Square(5);
-        assert s.area() == 25;
+        Assert.assertEquals(s.area(), 25.0);
     }
 }
