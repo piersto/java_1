0e98dd778b87ac32e4975dcbd927ee899a46bac9
piersto <44554156+piersto@users.noreply.github.com>
2021-02-02 23:15:33 -0500

    Added distance to the class Point.java

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,32 +6,39 @@ public class MyFirstProgram {
         hello("world");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l  + " = " + s.area());
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
 
-        double x1=0;
-        double y1=-0;
-        double x2=-3;
-        double y2=4;
+        Point p1 = new Point(-4, 10);
+        Point p2 = new Point(0, 13);
+        double d = p1.distance(p2);
+        System.out.println("Distance equal: "  + d);
+
+        double x1 = 0;
+        double y1 = -0;
+        double x2 = -3;
+        double y2 = 4;
         System.out.println("Distance equal: " + distance0(x1, x2, y1, y2));
 
 
-        Point p1 = new Point(11, 5);
-        Point p2 = new Point(12, 5);
-        System.out.println("Distance equal: " + distance(p1, p2));
+        Point p11 = new Point(11, 5);
+        Point p22 = new Point(12, 5);
+        System.out.println("Distance equal: " + distance2(p11, p22));
     }
 
-    public static void hello(String somebody){
+
+
+    public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "! РАЗ два три! € ");
     }
 
-    public static double distance(Point p1, Point p2) {
+    public static double distance2(Point p1, Point p2) {
         return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     }
 
-    public static double distance0(double x1, double x2, double y1, double y2){
-        return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    public static double distance0(double x1, double x2, double y1, double y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 }
--- sendbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,4 +8,8 @@ public class Point{
         this.x = x;
         this.y = y;
     }
+
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) *(p2.x - this.x) + (p2.y - this.y) *(p2.y - this.y));
+    }
 }
