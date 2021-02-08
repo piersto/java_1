e47933de8df111018831499bb858032a1af99486
piersto <44554156+piersto@users.noreply.github.com>
2021-02-08 17:29:50 -0500

    Лекция 2.8. Создаём общий базовый класс для классов-помощников. Moved isAllertPresent to HelperBase.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -35,15 +34,6 @@ public class ApplicationManager {
             wd.findElement(by);
             return true;
         } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
             return false;
         }
     }
