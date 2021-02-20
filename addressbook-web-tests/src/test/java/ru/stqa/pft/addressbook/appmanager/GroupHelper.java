3ded931089106db1ca09e0c3ea33fb2c75cbdace
piersto <44554156+piersto@users.noreply.github.com>
2021-02-19 20:41:50 -0500

    Лекция 4.3. Количество элементов в коллекции Added verification of the size to GroupCreationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -55,4 +55,9 @@ public class GroupHelper extends HelperBase {
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
     }
+
+    public int getGroupCount() {
+
+        return wd.findElements(By.name("selected[]")).size();
+    }
 }
