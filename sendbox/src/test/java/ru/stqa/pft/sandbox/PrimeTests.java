dbe5e919e4b4182ebe7644a0356c605ad45ad047
piersto <44554156+piersto@users.noreply.github.com>
2021-02-20 21:01:44 -0500

    Лекция 4.1. Циклы: многократные повторения похожих действий

---
+++ sendbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests
+{
+    @Test
+    public void testPrimes() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+        }
+
+    @Test
+    public void testNonPrimes() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
+    }
+
+    @Test(enabled = false)
+    public void testPrimesLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertFalse(Primes.isPrimeLong(n));
+    }
+
+}
