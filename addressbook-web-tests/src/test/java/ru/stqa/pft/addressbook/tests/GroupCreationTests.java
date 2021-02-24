980651c7c309c30cbfee9fc8ccfeb6b9a4c65551
piersto <44554156+piersto@users.noreply.github.com>
2021-02-23 21:41:52 -0500

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования Added for GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,17 +21,15 @@ public class GroupCreationTests extends TestBase {
         // Сравниваем пока только размеры списков:
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max =  0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-            max = g.getId();
-        }
-    }
-        group.setId(max);
+/*      Список превращаем в поток - stream, по нему пробегает функция и находит макс элемент,
+        при этом сравниваются объекты типа GroupData, путём сравнения их id.
+        На выходе этой функции будет макс объект - то есть группа с максимальным id.
+        И нам остаётся только взять её id.
+ */
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 
         // Добавляем в старый список ту группу, которую мы только что добавили в приложение
         before.add(group);
-
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
