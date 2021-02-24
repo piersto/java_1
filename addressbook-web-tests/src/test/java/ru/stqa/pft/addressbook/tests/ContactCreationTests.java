90f87e2e0cadb5d186d750e81a73f7c1f940225a
piersto <44554156+piersto@users.noreply.github.com>
2021-02-24 16:26:34 -0500

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования Added comparator to ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -30,7 +31,16 @@ public class ContactCreationTests extends TestBase {
                 max = c.getId();
             }
         }
-        contact.setId(max);
+
+        Comparator<? super ContactData> byId = new Comparator<ContactData>() {
+            @Override
+            public int compare(ContactData o1, ContactData o2) {
+                return Integer.compare(o1.getId(), o2.getId());
+            }
+        };
+        int max1 = after.stream().max(byId).get().getId();
+
+        contact.setId(max1);
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
