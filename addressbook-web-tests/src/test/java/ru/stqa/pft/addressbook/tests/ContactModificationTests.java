b5e195c2e7abccf40d17066fec07d7aa74a28292
piersto <44554156+piersto@users.noreply.github.com>
2021-02-24 22:09:30 -0500

    Лекция 4.10. Сортировка списков Done for ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,12 +4,46 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 
 public class ContactModificationTests extends TestBase {
 
+
+    @Test
+    public void testModifyContactSorted() {
+        if (! app.getContactHelper().isThereAContact())
+        {
+            app.getContactHelper().initContactCreation();
+            app.getContactHelper().createContact(new ContactData("Masha",
+                    null, "Petrova", null,
+                    null, null, null,
+                    null, "[none]"), true);
+            app.getNavigationHelper().returnToHomePage();
+        }
+        List<ContactData> before = app.getContactHelper().getContactList();
+        // app.getContactHelper().selectContact(before.size() - 1);
+        app.getContactHelper().initContactModification(before.size() - 1);
+        // Делаем локальную переменную, так как использовать будем не один раз
+        ContactData contact = new ContactData(before.get(before.size() - 1).getId(),"Maria",
+                null,  "Ivanova", null, null, null,
+                null, null, "[none]");
+
+        app.getContactHelper().fillContactForm(contact, false);
+        app.getContactHelper().submitContactModification();
+        app.getNavigationHelper().returnToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(contact);
+
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        // Сравниваем упорядоченные списки
+        Assert.assertEquals(before, after);
+    }
 
     @Test
     public void testModifyContact() {
