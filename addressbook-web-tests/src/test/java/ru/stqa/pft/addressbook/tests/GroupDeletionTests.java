ae02ac8cca142ecf962aae64b785f518e1925530
piersto <44554156+piersto@users.noreply.github.com>
2021-02-25 20:12:49 -0500

    Лекция 5.2. Улучшение кода тестов: ещё немного реструктуризации кода Extracted to BeforeMethod - verification in GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,12 +9,16 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-    @Test
-    public void testDeleteGroup() {
+    @BeforeMethod
+    public void insurePreconditions() {
         app.getNavigationHelper().goToGroupPage();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "header1", "footer1"));
         }
+    }
+
+    @Test
+    public void testDeleteGroup() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
