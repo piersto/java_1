ab82993e71b45de6d0c786858dca545290b574f4
piersto <44554156+piersto@users.noreply.github.com>
2021-03-01 20:56:49 -0500

    Лекция 5.5. Повсеместное использование уникальных идентификаторов объектов Changed list to all in preconditions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -12,7 +12,7 @@ public class ContactModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
-        if (app.contact().list().size() == 0)
+        if (app.contact().all().size() == 0)
         {
             app.contact().initContactCreation();
             app.contact().create(new ContactData().
