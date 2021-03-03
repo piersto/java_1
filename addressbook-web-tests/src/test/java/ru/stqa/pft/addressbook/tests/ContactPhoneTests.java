291080eea4ef8e8180bafe5fca5286ec913f8fae
piersto <44554156+piersto@users.noreply.github.com>
2021-03-03 16:36:43 -0500

    Лекция 5.9. Способы построения сложных локаторов Added ContactPhoneTests and to it @BeforeMethod

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactPhoneTests extends TestBase {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.contact().all().size() == 0) {
+            app.contact().initContactCreation();
+            app.contact().create(new ContactData().
+                    withFirstname("Masha").
+                    withLastname("Petrova").
+                    withHomephone("111").
+                    withGroup("[none]"), true);
+            app.goTo().homePage();
+        }
+    }
+
+    @Test
+    public void testContactPhones() {
+        app.goTo().homePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
