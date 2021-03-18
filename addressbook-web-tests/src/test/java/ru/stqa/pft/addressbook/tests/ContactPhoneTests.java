994d78e9e37ce0cac15a5d6d63bcf949227f2d46
piersto <44554156+piersto@users.noreply.github.com>
2021-03-18 17:09:48 -0400

    Лекция 7.6. Связи между объектами Fixed ContactPhoneTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -1,28 +1,29 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
+        Groups groups = app.db().groups();
+
         if (app.contact().all().size() == 0) {
             app.contact().initContactCreation();
             app.contact().create(new ContactData().
                     withFirstname("Masha").
                     withLastname("Petrova").
                     withHomephone("111").
-                    withGroup("[none]"), true);
+                    inGroup(groups.iterator().next()), true);
             app.goTo().homePage();
         }
     }
