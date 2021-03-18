258005ee64c7ebbfbc11a26b4ea1f9cf93ab356c
piersto <44554156+piersto@users.noreply.github.com>
2021-03-18 15:09:58 -0400

    Лекция 7.5. Отключаемые проверки UI Done for GroupModificationDbTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -50,12 +50,13 @@ public class TestBase {
     }
 
     public void verifyGroupListInUi() {
-
-        Groups dbGroups = app.db().groups();
-        Groups uiGroups = app.group().all();
-        assertThat(uiGroups, equalTo(dbGroups.stream()
-                        .map((g) -> new GroupData().withId(g.getId())
-                        .withName(g.getName()))
-                        .collect(Collectors.toSet())));
+        if(Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId())
+                            .withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
     }
 }
