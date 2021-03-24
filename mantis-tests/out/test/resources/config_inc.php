cac333a1678d2c2843fc38bd8daf08738616f1a2
piersto <44554156+piersto@users.noreply.github.com>
2021-03-23 22:46:51 -0400

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP LoginTests passed. Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP Test passed but looks like wrong page is opened. TBV

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -4,6 +4,6 @@ $g_db_type                = 'mysqli';
 $g_database_name          = 'bugtracker';
 $g_db_username            = 'root';
 $g_db_password            = '';
-
 $g_default_timezone       = 'Europe/Berlin';
-//$g_signup_use_capture     = OFF;
+$g_crypto_master_salt     = '/cGHjFFoeczvq7ucclaURiLZetsxeFqSFDLgDldIu5o=';
+$g_signup_use_capture     = OFF;
