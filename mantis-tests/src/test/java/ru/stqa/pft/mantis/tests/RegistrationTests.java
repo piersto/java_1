fac58e255a85c3a52625e3f155fdf65586098095
piersto <44554156+piersto@users.noreply.github.com>
2021-03-27 17:20:27 -0400

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер Added FindConfirmationLink and more

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -3,6 +3,12 @@ package ru.stqa.pft.mantis.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
 public class RegistrationTests extends TestBase{
 
@@ -12,9 +18,22 @@ public class RegistrationTests extends TestBase{
     }
 
     @Test
-    public void testRegistration() {
-        app.registration().start("user1", "user1@localhost.localdomain");
+    public void testRegistration() throws IOException, MessagingException {
+        String email = "user1@localhost.localdomain";
+        app.registration().start("user1", email);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 30000);
+        findConfirmationLink(mailMessages, email);
 
+
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+
+        // Сначала нужно найти среди всех писем то, которое было отправлено на нужный адрес: mailMessages.stream()
+        MailMessage mailMessage = mailMessages.stream()
+                .filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
     }
 
     // alwaysRun = true чтобы сервер останавливался даже если тест упал
