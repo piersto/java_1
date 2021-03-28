fb1f1a17743bed5184981d62354efac40e12790c
piersto <44554156+piersto@users.noreply.github.com>
2021-03-28 17:49:20 -0400

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер Added currentTimeMillis to the name and mail of the user

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -5,11 +5,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
-
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
-
 import static org.testng.AssertJUnit.assertTrue;
 
 public class RegistrationTests extends TestBase{
@@ -21,18 +19,16 @@ public class RegistrationTests extends TestBase{
 
     @Test
     public void testRegistration() throws IOException, MessagingException {
-
-        String user = "user4";
+        long now = System.currentTimeMillis();
+        String user = String.format("user%s", now);
         String password = "password";
-        String email = "user4@localhost.localdomain";
+        String email = String.format("user%s@localhost.localdomain", now);
 
         app.registration().start(user, email);
         List<MailMessage> mailMessages = app.mail().waitForMail(2, 30000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
-
-
     }
 
     private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
