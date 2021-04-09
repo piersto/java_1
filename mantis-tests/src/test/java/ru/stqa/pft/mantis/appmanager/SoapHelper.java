3a2d7438609f447c612c9d101e97d6a2cd64d9a6
piersto <44554156+piersto@users.noreply.github.com>
2021-04-08 21:15:29 -0400

    Лекция 9.3. SOAP: взаимодействие с MantisBT testCreateIssue passed

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import biz.futureware.mantis.rpc.soap.client.*;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -22,12 +22,36 @@ public class SoapHelper {
     }
 
     public Set<Project> getProjects() throws MalformedURLException, ServiceException, RemoteException {
-        MantisConnectPortType mc = new MantisConnectLocator()
-                .getMantisConnectPort
-                        (new URL("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php"));
+        MantisConnectPortType mc = getMantisConnect();
         ProjectData[] projects = mc.mc_projects_get_user_accessible
                 ("administrator", "root");
                 return Arrays.asList(projects).stream().map((p) -> new Project()
                 .withId(p.getId().intValue()).withName(p.getName())).collect(Collectors.toSet());
+    }
+
+    private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
+        MantisConnectPortType mc = new MantisConnectLocator()
+                .getMantisConnectPort
+                        (new URL("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php"));
+        return mc;
+    }
+
+    public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mc = getMantisConnect();
+        String[] categories = mc.mc_project_get_categories("administrator", "root",
+                BigInteger.valueOf(issue.getProject().getId()));
+        IssueData issueData = new IssueData();
+        issueData.setSummary(issue.getSummary());
+        issueData.setDescription(issue.getDescription());
+        issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()),
+                                                            issue.getProject().getName()));
+        issueData.setCategory(categories[0]);
+        BigInteger issueId = mc.mc_issue_add("administrator", "root", issueData);
+        IssueData  createdIssueData = mc.mc_issue_get("administrator", "root", issueId);
+        return new Issue().withId(createdIssueData.getId().intValue())
+                .withSummary(createdIssueData.getSummary())
+                .withDescription(createdIssueData.getDescription())
+                .withProject(new Project().withId(createdIssueData.getProject().getId().intValue())
+                .withName(createdIssueData.getProject().getName()));
     }
 }
