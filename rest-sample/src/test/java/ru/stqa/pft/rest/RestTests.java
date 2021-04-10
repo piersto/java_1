66f9157912448fa6a9a75e055d3bcf15ef4dd66e
piersto <44554156+piersto@users.noreply.github.com>
2021-04-10 15:44:49 -0400

    Лекция 9.4. REST: взаимодействие с Bugify Test get data via API

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,14 +1,17 @@
 package ru.stqa.pft.rest;
 
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.Set;
 
 public class RestTests {
 
     @Test
-    public void testCreateIssue() {
+    public void testCreateIssue() throws IOException {
         Set<Issue> oldIssues = getIssues();
         Issue newIssue = new Issue();
         int issueId = createIssue(newIssue);
@@ -18,8 +21,14 @@ public class RestTests {
 
     }
 
-    private Set<Issue> getIssues() {
+    private Set<Issue> getIssues() throws IOException {
+        String jason = getExecutor().execute(Request
+                .Get("http://demo.bugify.com/api/issues.json")).returnContent().asString();
         return null;
+    }
+
+    private Executor getExecutor() {
+        return  Executor.newInstance().auth("ad348266150ab4beafbfa5e67ec06368", "");
     }
 
     private int createIssue(Issue newIssue) {
