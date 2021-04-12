6846fa25c787b00f34036a38b72bc77cf89a26f8
piersto <44554156+piersto@users.noreply.github.com>
2021-04-11 21:09:36 -0400

    Лекция 9.6. REST: взаимодействие с GitHub Test changed to print out messages of the commits, not only links to commits like in previous

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -4,15 +4,18 @@ import com.google.common.collect.ImmutableBiMap;
 import com.jcabi.github.*;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class GithubTests {
 
     @Test
-    public void testCommits() {
+    public void testCommits() throws IOException {
         Github github = new RtGithub("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");
         RepoCommits commits = github.repos().get(new Coordinates.Simple("piersto", "java_1"))
                 .commits();
         for (RepoCommit commit : commits.iterate(new ImmutableBiMap.Builder<String, String>().build())) {
-            System.out.println(commit);
+            // Print out messages of the commits
+            System.out.println(new RepoCommit.Smart(commit).message());
         }
     }
 }
