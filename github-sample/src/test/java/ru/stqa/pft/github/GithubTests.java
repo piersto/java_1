76c9523ba0ba75a5dfa2598160f1777101c68d67
piersto <44554156+piersto@users.noreply.github.com>
2021-04-11 20:56:36 -0400

    Лекция 9.6. REST: взаимодействие с GitHub Added new module github-sample and started to write code

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() {
+        Github github = new RtGithub("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");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("piersto", "java_1"))
+                .commits();
+        for (RepoCommit commit : commits.iterate()) {
+            System.out.println(commit);
+        }
+
+
+    }
+}
