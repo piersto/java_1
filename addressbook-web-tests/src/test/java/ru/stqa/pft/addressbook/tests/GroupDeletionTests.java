052da524aede9ed0f0cf4b31640b70e6b1d87859
piersto <44554156+piersto@users.noreply.github.com>
2021-04-13 21:37:40 -0400

    Лекция 10.2. Удалённый запуск браузера (Selenium Server) Removed tests that anyway fail from GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -5,12 +5,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.List;
-import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -24,7 +20,7 @@ public class GroupDeletionTests extends TestBase {
     }
 
     @Test
-    public void testDeleteGroup56() {
+    public void testDeleteGroup() {
         app.goTo().groupPage();
         Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
@@ -32,44 +28,5 @@ public class GroupDeletionTests extends TestBase {
         assertThat(app.group().count(), equalTo(before.size() -1));
         Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(deletedGroup)));
-
     }
-
-
-
-
-
-
-
-
-
-
-
-    @Test(enabled = false)
-    public void testDeleteGroupSet() {
-        Set<GroupData> before = app.group().all();
-        GroupData deletedGroup = before.iterator().next();
-        app.group().delete(deletedGroup);
-        assertThat(app.group().count(), equalTo(before.size() - 1));
-        Set<GroupData> after = app.group().all();
-        // Удаляем из списка before группу с тем же идексом, что и у удалённой = before.size - 1
-        before.remove(deletedGroup);
-        // Добавили в GroupData метод equals и удалили из кода луп, так как этот метод сам умеет его делать
-        assertEquals(before, after);
-    }
-
-    @Test (enabled = false)
-    public void testDeleteGroupSortedList() {
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
-        app.group().delete(index);
-        List<GroupData> after = app.group().list();
-        assertEquals(after.size(), before.size() - 1);
-        // Удаляем из списка before группу с тем же идексом, что и у удалённой = before.size - 1
-        before.remove(index);
-        // Добавили в GroupData метод equals и удалили из кода луп, так как этот метод сам умеет его делать
-        assertEquals(before, after);
-    }
-
-
 }
