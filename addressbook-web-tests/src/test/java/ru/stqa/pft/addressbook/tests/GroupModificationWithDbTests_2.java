11b6f9e62bfef65708d71285fcb907a1804fed05
piersto <44554156+piersto@users.noreply.github.com>
2021-04-14 22:48:51 -0400

    Лекция 7.4. Сравнение данных, загруженных из БД Started second time. Test passed.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationWithDbTests_2.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GroupModificationWithDbTests_2 extends TestBase {
+
+    @BeforeMethod
+    public void insurePreconditions() {
+        if (app.db().groups().size() == 0)
+        {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("Group name"));
+        }
+    }
+
+    @Test
+    public void testModifyGroup_2() {
+        Groups before = app.db().groups();
+        GroupData modifiedGroup = before.iterator().next();
+        GroupData group = new GroupData().withId(modifiedGroup.getId()).
+                withName("group11").withHeader("header11").withFooter("footer11");
+        app.goTo().groupPage();
+        app.group().modify(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.db().groups();
+        assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
+    }
+}
