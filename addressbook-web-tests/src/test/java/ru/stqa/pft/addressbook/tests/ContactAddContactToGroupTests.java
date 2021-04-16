c40620a1e5e8150d02ee46ecff95eee43b82db82
piersto <44554156+piersto@users.noreply.github.com>
2021-04-15 21:30:53 -0400

    Задание №16: Реализовать тесты для добавления контакта в группу и удаления контакта из группы Restarted.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddContactToGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddContactToGroupTests.java
@@ -1,38 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactAddContactToGroupTests extends TestBase{
+public class ContactAddContactToGroupTests extends TestBase {
     private static SessionFactory sessionFactory;
 
-    @BeforeClass
-    protected void setUp2() throws Exception {
-        // A SessionFactory is set up once for an application!
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
-        try {
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
-        }
-    }
-/*
+
     @BeforeMethod
     public void ensurePreconditions() {
         if (app.db().contacts().size() == 0) {
@@ -51,46 +33,8 @@ public class ContactAddContactToGroupTests extends TestBase{
 
     @Test
     public void testContactAddToGroup2() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        // Get Contacts list
-        List<ContactData> listOfContacts = session.createQuery
-                ( "from ContactData where deprecated = '0000-00-00'" ).list();        // Get Groups list
-        // Get Groups list
-        List<GroupData> listOFGroups = session.createQuery( "from GroupData" ).list();
-        // Iterate through listOfContacts to get groups in every contact
-        for (ContactData contact : listOfContacts) {
-            List<Integer> contactGroups = contact.getGroups();
-            // Iterate through the group to verify if group in contactGroups
-            for (GroupData group : listOFGroups) {
-                if (group.contains(contactGroups);
-                return;
-
-            }
-        }
-
-
-        // Iterate Contacts to get Contact's groups
-        // Iterate Groups to verify if group's id in Contact's groups Yes - pass, No - add
-
-
-        //Contacts before = app.db().contacts();
-        //ContactData modifiedContact = before.iterator().next();
-        //ContactData contact = new ContactData().withId(modifiedContact.getId());
-
-        /*
-        app.goTo().homePage();
-        // Select contact
-        app.contact().selectContactById(modifiedContact.getId());
-        // Click Add button
-        app.contact().addContactToGroup();
-        Contacts after = app.db().contacts();
-        Assert.assertEquals(after.size(), before.size());
-        assertThat(after, equalTo(before.without(contact).withAdded(modifiedContact)));
-
-
     }
- */
+
 
 
 
