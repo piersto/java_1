bed8473ce3978f1c5e1bc6edd120a9d001d826d1
piersto <44554156+piersto@users.noreply.github.com>
2021-04-22 22:32:37 -0400

    Задание №18: Реализовать тест для смены пароля в MantisBT Added old version of ChangePasswordToUser_0. Test passed.

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordToUser_0.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class ChangePasswordToUser_0 extends TestBase{
+
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
+    @Test
+    public void testChangePasswordToUser() throws IOException, MessagingException {
+
+        String email = "pstoiko@localhost.localdomain";
+        String password = "pass";
+        String user = "piersto";
+        app.admin().openLoginPage();
+        app.admin().login();
+        app.admin().openMangeUsersPage();
+        app.admin().initPasswordChange(user);
+        app.admin().logout();
+
+        // Найти среди всех писем то, которое было отправлено на нужный адрес,
+        // пройти по ссылке, изменить пароль
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 30000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+        assertTrue(app.newSession().login(user, password));
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        // Сначала нужно найти среди всех писем то, которое было отправлено на нужный адрес:
+        // mailMessages.stream()
+        MailMessage mailMessage = mailMessages.stream()
+                .filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://")
+                .nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    // alwaysRun = true чтобы сервер останавливался даже если тест упал
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
+    }
+}
