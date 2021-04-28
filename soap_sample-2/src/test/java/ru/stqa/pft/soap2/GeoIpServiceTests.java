d18b35b69e970551ad9612662110ecc19cb961ed
piersto <44554156+piersto@users.noreply.github.com>
2021-04-27 21:05:15 -0400

    Лекция 9.2. SOAP: простой пример Started and left as package net.webservicex isn't generated. Commented code.

--- soap_sample-2/src/test/java/ru/stqa/pft/soap2/GeoIpServiceTests.java
+++ soap_sample-2/src/test/java/ru/stqa/pft/soap2/GeoIpServiceTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.soap2;
 
-import com.lavasoft.GeoIPService;
 import org.testng.annotations.Test;
 
 public class GeoIpServiceTests {
@@ -8,6 +7,7 @@ public class GeoIpServiceTests {
     @Test
 
     public void testMyIp() {
-        new GeoIPService().getGeoIPServiceSoap12().getGeoIp("45.42.102.219");
+       // new GeoIPService().getGeoIPServiceSoap12().
+                //getGeoIp("45.42.102.219");
     }
 }
