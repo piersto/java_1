c1ee52abf3c1038e8d4c891ec670227f11fdc7e8
piersto <44554156+piersto@users.noreply.github.com>
2021-05-03 16:56:21 -0400

    Задание №20: Реализовать интеграцию тестов с баг-трекером Bugify Refactored code: added model, tests, appmanager packages

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/RestTests.java
@@ -1,24 +1,20 @@
-package ru.stqa.pft.rest;
+package ru.stqa.pft.rest.tests;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.rest.model.Issue;
 
 import java.io.IOException;
 import java.util.Set;
 
-public class RestAssuredTests {
-
-    @BeforeClass
-    public void init() {
-        RestAssured.authentication = RestAssured.basic
-                ("288f44776e7bec4bf44fdfeb1e646490", "");
-    }
+public class RestTests {
 
     @Test
     public void testCreateIssue() throws IOException {
@@ -33,18 +29,24 @@ public class RestAssuredTests {
     }
 
     private Set<Issue> getIssues() throws IOException {
-        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+        String json = getExecutor().execute(Request
+                .Get("https://bugify.stqa.ru/api/issues.json")).returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
         return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
     }
 
-    private int createIssue(Issue newIssue) throws IOException {
-        String json = RestAssured.given()
-                            .parameter("subject", newIssue.getSubject())
-                            .parameter("description", newIssue.getDescription())
-                            .post("https://bugify.stqa.ru/api/issues.json").asString();
+    private Executor getExecutor() {
+        return  Executor.newInstance()
+                .auth("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
 
+    private int createIssue(Issue newIssue) throws IOException {
+        String json = getExecutor().execute(Request
+                .Post("https://bugify.stqa.ru/api/issues.json")
+                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                          new BasicNameValuePair("description", newIssue.getDescription())))
+                .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
     }
