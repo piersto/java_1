f109e473dfe513e4454de518171580f8243614be
piersto <44554156+piersto@users.noreply.github.com>
2021-05-04 21:07:11 -0400

    Задание №20: Реализовать интеграцию тестов с баг-трекером Bugify HomeWork20Test is ready to be executed

--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/HomeWork20Test.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/HomeWork20Test.java
@@ -1,13 +1,23 @@
 package ru.stqa.pft.rest.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.xml.rpc.ServiceException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class HomeWork20Test extends TestBase{
 
+    @BeforeMethod
+    public void skipIfNotFixed() throws MalformedURLException, ServiceException, RemoteException {
+        skipIfNotFixed(1048);
+    }
+
     @Test
     public void testCreateIssue() throws IOException {
-
+        System.out.println("Bug is fixed. За работу!");
     }
+
 }
--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -3,19 +3,26 @@ package ru.stqa.pft.rest.tests;
 import biz.futureware.mantis.rpc.soap.client.IssueData;
 import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
 import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.rest.appmanager.ApplicationManager;
+import ru.stqa.pft.rest.model.Issue;
 
 import javax.xml.rpc.ServiceException;
-import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.Set;
 
 public class TestBase {
 
@@ -27,26 +34,28 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite(alwaysRun = true)
-    public void tearDown() throws IOException {
-        app.stop();
+    private Executor getExecutor() {
+        return  Executor.newInstance()
+                .auth("288f44776e7bec4bf44fdfeb1e646490", "");
     }
 
-    public static boolean isIssueOpen(int issueId) throws MalformedURLException, RemoteException, ServiceException {
-        MantisConnectPortType mc = new MantisConnectLocator()
-                .getMantisConnectPort(new URL
-                        (app.getProperty("web.baseUrl") + "api/soap/mantisconnect.php"));
-        BigInteger id = new BigInteger(String.valueOf(issueId));
-        IssueData issueData = mc.mc_issue_get
-                (app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), id);
-        String status = issueData.getStatus().getName();
-        System.out.println(status);
-        return !status.equals("closed");
+    private Set<Issue> getIssues() throws IOException {
+        String json = getExecutor().execute(Request
+                .Get("https://bugify.stqa.ru/api/issues.json")).returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
     }
+
 
     public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
         if (isIssueOpen(issueId)) {
             throw new SkipException("Ignored because of issue " + issueId);
         }
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws IOException {
+        app.stop();
     }
 }
