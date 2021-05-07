d1fe55666f4752b1e7a31a1af962e59fa51a70e1
piersto <44554156+piersto@users.noreply.github.com>
2021-05-07 18:31:08 -0400

    Задание №20: Реализовать интеграцию тестов с баг-трекером Bugify Done

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/model/IssueWrapper.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.rest.model;
+
+import java.util.List;
+
+public class IssueWrapper {
+    private  int total;
+    private  int page;
+    private  int limit;
+    private List <Issue> issues;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<Issue> issues) {
+        this.issues = issues;
+    }
+}
--- rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/tests/TestBase.java
@@ -3,14 +3,18 @@ package ru.stqa.pft.rest.tests;
 import com.google.gson.Gson;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.rest.appmanager.ApplicationManager;
 import ru.stqa.pft.rest.model.Issue;
+import ru.stqa.pft.rest.model.IssueWrapper;
 
 import java.io.IOException;
+import java.io.NotActiveException;
+import java.util.List;
 
 public class TestBase {
 
@@ -32,10 +36,16 @@ public class TestBase {
                 .Get(app.getProperty("web.baseUrl") + "api/issues/" + issueId + ".json")).returnContent().asString();
         System.out.println("This is my json: " + issueJson);
         Gson gson = new Gson();
-        Issue issue = gson.fromJson(issueJson, Issue.class);
+        IssueWrapper issueWrapper = gson.fromJson(issueJson, IssueWrapper.class);
+        List<Issue> issues = issueWrapper.getIssues();
+        if (issues == null)
+            throw new NotFoundException();
+        Issue issue = issues.iterator().next();
+        if (issue == null)
+            throw new NotActiveException();
         String state = issue.getState_name();
-        System.out.println("This is my state: " + state);
-        return !state.equals("Closed");
+        System.out.println("This is my state: " );
+        return ! "Closed".equals(state);
     }
 
     public void skipIfNotFixed(int issueId) throws IOException {
