90e8b440fd31d346c31544244e929817b6758893
piersto <44554156+piersto@users.noreply.github.com>
2021-05-17 20:58:12 -0400

    Задание №16: Реализовать тесты для добавления контакта в группу и удаления контакта из группы Added new contact in precondition.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddContactToGroupTests3.java
@@ -0,0 +1,51 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hibernate.SessionFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddContactToGroupTests3 extends TestBase {
+    private static SessionFactory sessionFactory;
+    private ContactData modifiedContact;
+
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.contact().initContactCreation();
+        modifiedContact = new ContactData()
+                .withFirstname("Masha")
+                .withLastname("Petrova");
+        app.contact().create(modifiedContact, true);
+        app.goTo().homePage();
+
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("Group1"));
+            app.goTo().homePage();
+        }
+    }
+
+    @Test
+    public void testContactAddToGroup() {
+        Contacts before = app.db().contacts();
+        ContactData modifiedContact = before.iterator().next();
+        ContactData contact = new ContactData().withId(modifiedContact.getId());
+        app.goTo().homePage();
+        // Select contact
+        app.contact().selectContactById(modifiedContact.getId());
+        // Click Add button
+        app.contact().addContactToGroup();
+        Contacts after = app.db().contacts();
+        Assert.assertEquals(after.size(), before.size());
+        assertThat(after, equalTo(before.without(contact).withAdded(modifiedContact)));
+    }
+
+}
