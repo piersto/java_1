fbaf195f0120624d99e0ee3179cbd52a7085d18c
piersto <44554156+piersto@users.noreply.github.com>
2021-05-25 21:12:22 -0400

    Задание №16: Реализовать тесты для добавления контакта в группу и удаления контакта из группы Started to add precondition to the ContactRemoveFromGroupTests3

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactRemoveFromGroupTests3.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hibernate.SessionFactory;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+public class ContactRemoveFromGroupTests3 extends TestBase {
+
+    private static SessionFactory sessionFactory;
+
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+
+        if (app.db().contacts().size() == 0) {
+            app.contact().initContactCreation();
+            app.contact().create(new ContactData()
+                    .withFirstname("Masha")
+                    .withLastname("Petrova"), true);
+            app.goTo().homePage();
+        }
+
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("Group1"));
+            app.goTo().homePage();
+        }
+
+        Contacts contacts = app.db().contacts();
+        Groups groups = app.db().groups();
+
+
+
+    }
+}
