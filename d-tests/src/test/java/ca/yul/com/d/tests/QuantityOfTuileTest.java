98e173072d69e3190b8f3613035766d5880931a5
piersto <44554156+piersto@users.noreply.github.com>
2021-10-11 19:37:47 -0400

    Renamed to QuantityOfTuileTest

--- d-tests/src/test/java/ca/yul/com/d/tests/QuantityOfTuileTest.java
+++ d-tests/src/test/java/ca/yul/com/d/tests/QuantityOfTuileTest.java
@@ -3,10 +3,10 @@ package ca.yul.com.d.tests;
 import ca.yul.com.d.appmanager.CarriereHelper;
 import org.testng.annotations.Test;
 
-public class QuantityOfBlaBlaBlaTest extends TestBase {
+public class QuantityOfTuileTest extends TestBase {
 
     @Test
-    public void testQuantityOFBlaBlaBla() throws InterruptedException {
+    public void testQuantityOFTuiles() throws InterruptedException {
         app.carriere().openCarrierePage();
         Thread.sleep(50);
         int count = app.carriere()
